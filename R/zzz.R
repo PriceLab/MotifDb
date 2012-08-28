b70c1a6e72fa18219b29d523dd7209ba494eff53
m.carlson <m.carlson@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-08-28 17:36:36 +0000

    Adds PADOG, RMassBank, ChIPXpress, MotifDb, and GeneNetworkBuilder to the repos.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MotifDb@68872 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/zzz.R
@@ -0,0 +1,36 @@
+MotifDb <- NULL
+#-------------------------------------------------------------------------------
+.MotifDb = function (loadAllSources=TRUE, quiet=TRUE)
+{
+  mdb = MotifList ()
+
+  if (loadAllSources) {
+    data.path = system.file ('extdata', package='MotifDb') # e (system.file (package='MotifDb'), 'data', sep='/')
+    data.files = dir (data.path, full.names=TRUE)
+  
+    if (length (data.files) > 0)   
+      for (data.file in data.files) {
+         # define these to keep 'check' happy.  they are loaded by 'load'
+        tbl.md = NA; matrices = NA;  
+        variables = load (data.file)
+        mdb = append (mdb, MotifList (matrices, tbl.md))
+        if (!quiet) 
+          message (noquote (sprintf ('added %s (%d) matrices, length now: %d',
+                   basename (data.file), length (matrices), length (mdb))))
+      } # for data.file
+  
+    if (!quiet) {
+      print (table (values (mdb)$dataSource))
+      }
+    } # if loadAllSources
+
+  return (mdb)
+
+} # MotifDb
+#-------------------------------------------------------------------------------
+.onLoad <- function(libname, pkgname)
+{
+    MotifDb <<- .MotifDb (loadAllSources=TRUE, quiet=TRUE)
+}
+#-------------------------------------------------------------------------------
+
