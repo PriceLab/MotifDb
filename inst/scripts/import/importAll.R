abcbfdb378a340581b19c37ce6d658cfe23facd7
p.shannon <p.shannon@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-04-05 16:50:33 +0000

    disabled automatic testing
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MotifDb@75356 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/import/importAll.R
+++ inst/scripts/import/importAll.R
@@ -5,8 +5,8 @@ stopifnot(basename(starting.directory) == "import")
 for(directory in directories){
     print(noquote(sprintf("--- importing %s", directory)))
     setwd(file.path(starting.directory, directory))
-    source("test.R")
-    run.tests()
+    #source("test.R")
+    #run.tests()
     source("import.R")
     run()
     }
