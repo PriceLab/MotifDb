b52fba587c30e8750d59174e0c69976fd9b43af5
p.shannon <p.shannon@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-04-05 20:39:49 +0000

    added repoRoot assignment, only mention in all files
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MotifDb@75384 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/import/importAll.R
+++ inst/scripts/import/importAll.R
@@ -2,15 +2,15 @@ directories <- c("flyFactorSurvey", "hPDI", "jaspar", "ScerTF", "stamlab")#   "u
 starting.directory <- getwd()
 stopifnot(basename(starting.directory) == "import")
 
-kDataDir <- "~/s/data/public/TFBS"
-kDataDir <- "/shared/silo_researcher/Morgan_M/BioC/MotifDb"
+repoRoot <- "~/s/data/public/TFBS"
+repoRoot <- "/shared/silo_researcher/Morgan_M/BioC/MotifDb"
 
 for(directory in directories){
     print(noquote(sprintf("--- importing %s", directory)))
     setwd(file.path(starting.directory, directory))
     source("test.R")
-    run.tests(kDataDir)
+    run.tests(repoRoot)
     source("import.R")
-    run(kDataDir)
+    run(repoRoot)
     }
     
