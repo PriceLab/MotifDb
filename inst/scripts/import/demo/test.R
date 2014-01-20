4c9d018cb6685daca8794e887a6b2d560d235554
Paul Shannon <pshannon@fhcrc.org>
2014-01-20 21:34:09 +0000

    good progress towards a simple MotifDb import demo.  needs cleanup, formatting, use by others
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MotifDb@85638 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/import/demo/test.R
+++ inst/scripts/import/demo/test.R
@@ -1,4 +1,4 @@
-# example/test.R
+# MotifDb/inst/scripes/import/demo/test.R
 #------------------------------------------------------------------------------------------------------------------------
 library (RUnit)
 #------------------------------------------------------------------------------------------------------------------------
@@ -6,13 +6,21 @@ source("import.R")
 #------------------------------------------------------------------------------------------------------------------------
 run.tests = function ()
 {
+  test.parsePwm()
   matrices.raw <- test.readRawMatrices (dataDir="./")
-  matrices <- test.extractAndNormalizeMatrices (matrices.raw)
-  #tbl.md <- test.createMetadataTable (matrices)
-  #matrices.renamed <- test.renameMatrices (matrices, tbl.md)
+  matrices <- test.extractMatrices(matrices.raw)
+
+  test.ncbiTaxonimicCodeToLinnaean()
+  test.guessProteinIdentifierType()
+
+  tbl.md <- test.createMetadataTable(dataDir="./", matrices, "md-raw.tsv")
+  matrices <- test.normalizeMatrices(matrices)
+  matrices.renamed <- test.renameMatrices(matrices, tbl.md)
 
 } # run.tests
 #------------------------------------------------------------------------------------------------------------------------
+# create representative text here, a list of 5 lines, and make sure parsePWM can translate it into
+# a one-element list, a 4 x 6 matrix (4 nucleotides by 6 positions) appropriately named.
 test.parsePwm <- function()
 {
     print("--- test.parsePwm")
@@ -35,7 +43,7 @@ test.parsePwm <- function()
 #------------------------------------------------------------------------------------------------------------------------
 test.readRawMatrices = function (dataDir)
 {
-  print ('--- test.readMatrices')
+  print ('--- test.readRawMatrices')
   list.pwms = readRawMatrices (dataDir)
   checkEquals (length (list.pwms), 4)
   checkEquals (names (list.pwms [[1]]), c ("title", "matrix"))
@@ -44,11 +52,30 @@ test.readRawMatrices = function (dataDir)
 
 } # test.readRawMatrices
 #------------------------------------------------------------------------------------------------------------------------
-test.extractAndNormalizeMatrices = function (matrices.raw)
+test.extractMatrices = function (matrices.raw)
 {
-  print ('--- test.extractAndNormalizeMatrices')
+  print ('--- test.extractMatrices')
 
-  matrices.fixed <- extractAndNormalizeMatrices(matrices.raw)
+  matrices.fixed <- extractMatrices(matrices.raw)
+  checkEquals(length(matrices.fixed), 4)
+
+  checkEquals(names(matrices.fixed),
+            c("MA0004.1 Arnt", "MA0006.1 Arnt::Ahr",  "MA0008.1 HAT5",
+              "MA0009.1 T"))
+
+    # make sure all columns in all matrices sum to 1.0
+  #checkTrue (all(sapply(matrices.fixed,
+  #                       function(m)all(abs(colSums(m)-1.0) < 1e-10))))
+
+  invisible (matrices.fixed)
+
+} # test.extractMatrices
+#------------------------------------------------------------------------------------------------------------------------
+test.normalizeMatrices = function (matrices)
+{
+  print ('--- test.normalizeMatrices')
+
+  matrices.fixed <- normalizeMatrices(matrices)
   checkEquals(length(matrices.fixed), 4)
 
   checkEquals(names(matrices.fixed),
@@ -61,64 +88,37 @@ test.extractAndNormalizeMatrices = function (matrices.raw)
 
   invisible (matrices.fixed)
 
-} # test.extractAndNormalizeMatrices
+} # test.extracNormalizeMatrices
 #------------------------------------------------------------------------------------------------------------------------
-test.convertRawMatricesToStandard = function (tbl.rmat)
+test.createMetadataTable = function (dataDir, matrices, raw.metadata.filename)
 {
-  print ('--- test.convertRawMatricesToStandard')
-   # get just the first two raw matrices
+    print ('--- test.createMetadataTable')
 
-  first.two.ids = head (unique (tbl.rmat$id), n=2)
-  rows = nrow (subset (tbl.rmat, id %in% first.two.ids))
-  matrices = convertRawMatricesToStandard (tbl.rmat [1:rows,])
-  checkEquals (length (matrices), 2)
-  checkEquals (names (matrices), first.two.ids)
+       # try it with just the first matrix
+    tbl.md = createMetadataTable (dataDir, matrices, raw.metadata.filename)
 
-    # it will not always be true, but IS true for the first two matrices, currently "9229" and  "9231", that there
-    # are an equal number of nucleotides at each position.
-  checkTrue (all (colSums (matrices [[1]]) == 97))
-  checkTrue (all (colSums (matrices [[2]]) == 185))
+    checkEquals (dim (tbl.md), c (length(matrices), 15))
+    checkEquals (colnames (tbl.md), c ("providerName", "providerId", "dataSource", "geneSymbol", "geneId", "geneIdType", 
+                                       "proteinId", "proteinIdType", "organism", "sequenceCount", "bindingSequence",
+                                       "bindingDomain", "tfFamily", "experimentType", "pubmedID"))
 
-    # now run all the matrices through
-  matrices = convertRawMatricesToStandard (tbl.rmat)
-  checkEquals (length (matrices), 459)
-  checkEquals (names (matrices)[1:2], first.two.ids)
-  
-  invisible (matrices)
-  
-} # test.convertRawMatricesToStandard 
-#------------------------------------------------------------------------------------------------------------------------
-test.createAnnotationTable = function ()
-{
-  print ('--- test.createAnnotationTable')
-  tbl.anno = createAnnotationTable ()
-  checkEquals (dim (tbl.anno), c (513, 13))
-  expected = c ("fullID", "id", "category", "mID", "version", "binder", "speciesID", "proteinID", "family", "tax", "class", "pubmed", "type")
-  checkEquals (colnames (tbl.anno), expected)
-
-  checkEquals (head (tbl.anno$fullID),  c ("MA0001.1", "MA0003.1", "MA0004.1", "MA0005.1", "MA0006.1", "MA0006.1"))
-  invisible (tbl.anno)
-
-} # test.createAnnotationTable
-#------------------------------------------------------------------------------------------------------------------------
-test.createMetadataTable = function (x.matrices, x.novels)
-{
-  print ('--- test.createMetadataTable')
-   # try it first with just two matrices
-  tbl.md = createMetadataTable (x.matrices [1:12], x.novels [1:12])
-  checkEquals (dim (tbl.md), c (12, 15))
-  checkEquals (colnames (tbl.md), c ("providerName", "providerId", "dataSource", "geneSymbol", "geneId", "geneIdType", 
-                                     "proteinId", "proteinIdType", "organism", "sequenceCount", "bindingSequence",
-                                     "bindingDomain", "tfFamily", "experimentType", "pubmedID"))
-   checkEquals (tbl.md$providerName [1:2], c ('UW.Motif.0001', 'UW.Motif.0002'))
-   checkEquals (tbl.md$providerId [1:2], c ('UW.Motif.0001', 'UW.Motif.0002'))
-   checkEquals (tbl.md$pubmedID [1:2], c ('22959076', '22959076'))
-   checkEquals (tbl.md$dataSource [1:2], c ('stamlab', 'stamlab'))
-   checkEquals (tbl.md$organism [1:2], c ('Hsapiens', 'Hsapiens'))
-   checkEquals (tbl.md$experimentType [1:2], c ('digital genomic footprinting', 'digital genomic footprinting'))
-   checkEquals (tbl.md$geneId, c (rep ('knownMotif', 11), 'novelMotif'))
-
-  invisible (tbl.md)
+    with(tbl.md[1,], checkEquals(providerName,   "MA0004.1 Arnt"),
+                     checkEquals(providerId,     "MA0004.1 Arnt"),
+                     checkEquals(dataSource,     "jaspar2014"),
+                     checkEquals(geneSymbol,     "Arnt"),
+                     checkEquals(proteinId,      "P53762"),
+                     checkEquals(proteinIdType,  "uniprot"),
+                     checkEquals(organism,       "Mus musculus"),
+                     checkEquals(sequenceCount,  20),
+                     checkEquals(tfFamily,       "Helix-Loop-Helix"),
+                     checkEquals(experimentType, "SELEX"),
+                     checkEquals(pubmedID,       "24194598"),
+                     checkTrue(is.na(geneId)),
+                     checkTrue(is.na(geneIdType)),
+                     checkTrue(is.na(bindingSequence)),
+                     checkTrue(is.na(bindingDomain)))
+             
+    invisible (tbl.md)
 
 } # test.createMetadataTable
 #------------------------------------------------------------------------------------------------------------------------
@@ -127,34 +127,21 @@ test.renameMatrices = function (matrices, tbl.md)
   print("--- test.renameMatrices")
   
     # try it with just the first two matrices
-  matrix.pair = matrices [1:2]
-  tbl.pair = tbl.md [1:2,]
-  matrix.pair.renamed = renameMatrices (matrix.pair, tbl.pair)
-  checkEquals (names (matrix.pair.renamed), c ("Hsapiens-stamlab-UW.Motif.0001", "Hsapiens-stamlab-UW.Motif.0002"))
+  matrix.pair <- matrices[1:2]
+  tbl.pair <- tbl.md[1:2,]
+  matrix.pair.renamed <- renameMatrices (matrix.pair, tbl.pair)
+  checkEquals (names (matrix.pair.renamed), 
+               c("Mus musculus-jaspar2014-MA0004.1 Arnt",
+                 "Mus musculus-jaspar2014-MA0006.1 Arnt::Ahr"))
 
 } # test.renameMatrices
 #------------------------------------------------------------------------------------------------------------------------
-test.convertTaxonCode = function ()
-{
-  print ('--- test.convertTaxonCode')
-
-  checkEquals (convertTaxonCode ('9606'), 'Hsapiens')
-  checkEquals (convertTaxonCode (9606), 'Hsapiens')
-     # anomalous codes, which an examination of the jaspar website reveals as 'vertebrates'
-  checkEquals (convertTaxonCode (NA), 'Vertebrata')
-  checkEquals (convertTaxonCode ('NA'), 'Vertebrata')
-  checkEquals (convertTaxonCode (NA_character_), 'Vertebrata')
-  checkEquals (convertTaxonCode ('-'), 'Vertebrata')
-
-} # test.convertTaxonCode
-#------------------------------------------------------------------------------------------------------------------------
-test.guessProteinIdentifierType = function (moleculeName)
+test.guessProteinIdentifierType <- function()
 {
   print ('--- test.guessProteinIdentifierType')
-  checkEquals (guessProteinIdentifierType ('P29383'), 'UNIPROT')
 
-  all.types = sapply (x.tbl.anno$proteinID, guessProteinIdentifierType)
-  checkTrue (length (which (is.na (all.types))) < 12)   # got most of them.
+  checkEquals (guessProteinIdentifierType ('P29383'), 'UNIPROT')
+  checkEquals (guessProteinIdentifierType ('NP_234234'), 'REFSEQ')
 
 } # test.guessProteinIdentifierType
 #------------------------------------------------------------------------------------------------------------------------
@@ -174,27 +161,15 @@ test.normalizeMatrices = function (matrices)
 
 } # test.normalizeMatrices
 #------------------------------------------------------------------------------------------------------------------------
-test.assignGeneId = function (proteinId)
+test.ncbiTaxonimicCodeToLinnaean <- function()
 {
-  print ('--- test.assignGeneId')
-  uniprot.ids = c ('Q9GRA5', 'P31314', 'AAC18941', 'O49397')
-  refseq.ids  = c ('NP_995315.1', 'NP_032840', 'NP_599022')
-  yeast.ids   = c ('YKL112W', 'YMR072W', 'YLR131C')
+    print("--- test.ncbiTaxonimicCodeToLinnaean")
 
-  checkEquals (assignGeneId ('NP_995315.1'), list (geneId='4782', type='ENTREZ'))
-  checkEquals (assignGeneId ('NP_599022'),   list (geneId='6095', type='ENTREZ'))
+    checkTrue(is.na(ncbiTaxonimicCodeToLinnaean("purplePeopleEater32")))
 
-  checkEquals (assignGeneId ('P31314'),      list (geneId='3195', type='ENTREZ'))
+    checkEquals(ncbiTaxonimicCodeToLinnaean("9606"), "Homo sapiens")
+    checkEquals(ncbiTaxonimicCodeToLinnaean(9606), "Homo sapiens")
+    checkEquals(ncbiTaxonimicCodeToLinnaean("10090"), "Mus musculus")
 
-  checkEquals (assignGeneId ('YKL112W'),     list (geneId='YKL112W', type='SGD'))
-
-    # see how successful this is over all 513 proteinIds
-
-  tbl.anno = createAnnotationTable ()
-  mtx.geneId = as.data.frame (t (sapply (tbl.anno$proteinID, assignGeneId)))
-  tbl.types = as.data.frame (table (as.character (mtx.geneId$type), useNA='always'), stringsAsFactors=FALSE)
-  checkEquals (tbl.types$Var1, c ("ENTREZ", "SGD", NA))
-  checkEquals (tbl.types$Freq, c (141, 177, 195))
-
-} # test.assignGeneId
-#------------------------------------------------------------------------------------------------------------------------
+} # test.ncbiTaxonimicCodeToLinnaean
+#-------------------------------------------------------------------------------
